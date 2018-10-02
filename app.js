2ce1ef91a21202c605f48d1d710eaf4d6f4a3a2f
santiagoanaya <anayasantiagohernan@gmail.com>
2018-10-02 15:11:45 -0300

    first comit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.send('Hello from B!');
+});
